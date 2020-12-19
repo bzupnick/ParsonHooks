26e668dfea33aab609cb105e8429cd6256c94c84
Binny Zupnick <binny@thirtymadison.com>
2020-12-19 14:54:25 -0500

    added all the models to the admin panel

--- src/admin.py
+++ src/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from src.models import User, Webhook, Message, Script, Secret
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Webhook)
+admin.site.register(Message)
+admin.site.register(Script)
+admin.site.register(Secret)
